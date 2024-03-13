79e405082cd2572c2eb133ae3eedd70c49770c37
JonathanTGold <jonathan.gold@86@gmail.com>
2024-03-13 14:23:12 +0100

    fix

--- src/skip.js
+++ src/skip.js
@@ -159,7 +159,6 @@ class Skip extends BasePlugin {
     this.player.currentTime = seekTo;
     this.dispatchEvent(SkipEvents.SKIP_BUTTON_CLICK, {mode: this._currentMode});
     this._removeButton();
-    this.dispatchEvent(SkipEvents.SKIP_BUTTON_CLICK, {mode: this._currentMode});
   }
 
   reset(): void {
