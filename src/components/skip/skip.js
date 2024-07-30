ca270934913e13b4133cc4f0644dbea856f8cc08
Tzipi-kaltura <tzipi.maksev@kaltura.com>
2024-07-30 17:49:23 +0300

    fix(ADA-1502): [By Applause] KN: Core Player: Skip Intro button is not activated by pressing Keyboard Enter/Spacebar

--- src/components/skip/skip.js
+++ src/components/skip/skip.js
@@ -12,6 +12,7 @@ const {PLAYER_SIZE} = ui.Components;
 const {bindActions} = ui.Utils;
 const {actions} = ui.Reducers.shell;
 const {connect} = ui.redux;
+const {KeyMap} = ui.utils;
 
 /**
  * mapping state to props
@@ -33,12 +34,26 @@ const COMPONENT_NAME = 'Skip';
  */
 @connect(mapStateToProps, bindActions(actions))
 class Skip extends Component {
+  /**
+   * on key down handler
+   *
+   * @param {KeyboardEvent} e - keyboard event
+   * @returns {void}
+   * @memberof Skip
+   */
+  onKeyDown = (e: KeyboardEvent): void => {
+    if (e.keyCode === KeyMap.ENTER || e.keyCode === KeyMap.SPACE) {
+      this.props.onClick();
+    }
+  };
+
   render(): React$Element<any> | void {
     if (this.props.playerSize !== PLAYER_SIZE.TINY && !this.props.loading) {
       return (
         <Localizer>
           <div
             onClick={this.props.onClick}
+            onKeyDown={this.onKeyDown}
             className={[
               skipStyle.btnSkip,
               this.props.parentComponent === 'InteractiveArea' ? skipStyle.interactiveAreaPosition : skipStyle.bottomBarPosition
